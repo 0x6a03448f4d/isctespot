0a810df2ae5919ef28fb75958ac35f2323d2e4ed
0x6a03448f4d <149377578+0x6a03448f4d@users.noreply.github.com>
2025-12-21 16:56:37 +0000

    fix defectdojo scripts & .gitignore

---
+++ scripts/upload_defectdojo.py
@@ -0,0 +1,24 @@
+import os
+from defectdojo_api import DefectDojoAPIv2
+
+dojo = DefectDojoAPIv2(
+    url=os.environ["DEFECTDOJO_URL"],
+    api_key=os.environ["DEFECTDOJO_API_KEY"],
+    verify_ssl=False
+)
+
+ENGAGEMENT_ID = int(os.environ["DEFECTDOJO_ENGAGEMENT_ID"])
+
+def upload(scan_type, file_path):
+    dojo.import_scan(
+        engagement_id=ENGAGEMENT_ID,
+        scan_type=scan_type,
+        file_path=file_path,
+        active=True,
+        verified=False
+    )
+upload("Trivy Dependencies", "trivy-dependencies.json")
+upload("Trivy Scan", "trivy-backend.json")
+upload("Trivy Scan", "trivy-frontend.json")
+upload("Dependency Check Scan", "dependency-check.json")
+upload("CycloneDX Scan", "sbom.json")
